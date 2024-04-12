eb6eaf4dab824f1938fe3ff7b96f11c5e8fcf50b
Eliezer Guevara <magdiel.852@gmail.com>
2024-04-12 12:02:47 -0600

    html estado_cuenta

--- Frontend_Django/app/views.py
+++ Frontend_Django/app/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -17,3 +18,17 @@ def ayuda(request):
 
 # --- pruebas
 
+def consultar_estado_cuenta(request):
+    if request.method == 'GET':
+        nit_cliente = request.GET.get('nit-cliente')
+
+        # Realizar la solicitud al servidor Flask
+        response = requests.get('http://127.0.0.1:5000/obtener-cliente', params={'nit-cliente': nit_cliente})
+
+        if response.status_code == 200:
+            data = response.json()  # Convertir la respuesta JSON en un diccionario de Python
+            return render(request, 'estado_cuenta.html', {'cliente_data': data})
+        else:
+            return HttpResponse('Error al obtener el cliente del servidor Flask', status=response.status_code)
+    else:
+        return HttpResponse('Método no permitido', status=405)
